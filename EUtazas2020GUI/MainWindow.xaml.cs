7f7637c32fbc198f711fd21b932d3e67fea3b3bd
Kevin <arpaskevin11@gmail.com>
2024-09-06 11:50:39 +0200

    WPF Segment soon to be finished. Some errors need to be fixed.

--- EUtazas2020GUI/MainWindow.xaml.cs
+++ EUtazas2020GUI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using Microsoft.VisualBasic;
+using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,13 +20,15 @@ namespace EUtazas2020GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string fileSrc = @"..\..\..\src\utasadat.txt";
         public MainWindow()
         {
             InitializeComponent();
-            takeOffDate_DatePicker.SelectedDate = DateTime.Now;
-            ticketValidityDate_Datepicker.SelectedDate = DateTime.Now;
+            //takeOffDate_DatePicker.SelectedDate = DateTime.Now;
+            //ticketValidityDate_Datepicker.SelectedDate = DateTime.Now;
+            leaseRadioButton.IsChecked = true;
 
-            string fileSrc = @"..\..\..\src\utasadat.txt";
+            
 
             var passengers = new List<Utas>();
 
@@ -33,13 +37,18 @@ namespace EUtazas2020GUI
                 passengers.Add(new(item));
             }
 
-            busStopCombobox.Items.Add("Válasszon megállót!");
-
-            busStopCombobox.SelectedIndex= 0;
+            busStopNumberCombobox.Items.Add("Válasszon megállót!"); busStopNumberCombobox.SelectedIndex= 0;
 
             foreach (var item in passengers.Select(e => e.BusStopNumber).Distinct())
             {
-                busStopCombobox.Items.Add(item);
+                busStopNumberCombobox.Items.Add(item);
+            }
+
+            ticketTypeCombobox.Items.Add("Válasszon megállót!"); ticketTypeCombobox.SelectedIndex = 0;
+
+            foreach (var item in passengers.Select(e => e.TicketType).Distinct())
+            {
+                ticketTypeCombobox.Items.Add(item);
             }
 
         }
@@ -49,9 +58,82 @@ namespace EUtazas2020GUI
             if (cardIDTextbox.Text != null) cardIdLabel.Content = $"{cardIDTextbox.Text.Length} db";
 
             cardIdLabel.Foreground = cardIDTextbox.Text.Length > 7 ? Brushes.Red : SystemColors.WindowTextBrush;
+        }   
+
+        private void usableTicketCountSlider_ValueChanged(object sender, EventArgs e)
+        {
+            //to be fixed 
         }
 
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var isLeaseChecked = (sender as RadioButton) == leaseRadioButton;
 
+            leaseGroupbox.Visibility = isLeaseChecked ? Visibility.Visible : Visibility.Collapsed;
+            ticketGroupbox.Visibility = isLeaseChecked ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void dataRecordButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedBusStopNumber = busStopNumberCombobox.SelectedItem;
+            var takeOffTime = takeOffTimeTextbox.Text;
+            var cardId = cardIDTextbox.Text;
+            var selectedLeaseType = ticketTypeCombobox.SelectedItem;
+
+            var busStopNumberPlaceholder = busStopNumberCombobox.Items[0];
+            if (selectedBusStopNumber == null || selectedBusStopNumber == busStopNumberPlaceholder)
+            {
+                MessageBox.Show("Nem választott megállót!", "Hiba!");
+                return;
+            }
+            if (takeOffDate_DatePicker.SelectedDate == null)
+            {
+                MessageBox.Show("Nem adott meg felszállási dátumot!", "Hiba!");
+                return;
+            }
+            if (!IsValidTimeFormat(takeOffTime))
+            {
+                MessageBox.Show("Időpont formátuma nem helyes! Használja a 'hh:mm' formátumot.", "Hiba!");
+                return;
+            }
+            if (cardId.Length != 7 || !int.TryParse(cardId, out _))
+            {
+                MessageBox.Show("Egy 7 számjegyet tartalmazó kártya azonosítót adjon meg.", "Hiba!");
+                return;
+            }
+            if (leaseRadioButton.IsChecked == true)
+            {
+                var ticketTypePlaceholder = ticketTypeCombobox.Items[0];
+
+                if (selectedLeaseType == null || selectedLeaseType == ticketTypePlaceholder)
+                {
+                    MessageBox.Show("Nem választott bérletet!", "Hiba!");
+                    return;
+                }
+            }
+            if (ticketValidityDate_Datepicker.SelectedDate == null)
+            {
+                MessageBox.Show("Nem adott meg bérlet dátumot!", "Hiba!");
+                return;
+            }
+
+            using StreamWriter sw = new(fileSrc);
+
+            //----------------------
+            //sw.WriteLine($"{selectedBusStopNumber} ");
+        }
+
+        private bool IsValidTimeFormat(string timeText)
+        {
+            // Regular expression for 'hh:mm' format
+            var regex = new Regex(@"^(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$");
+
+            if (!regex.IsMatch(timeText))
+                return false;
+
+            // Additional checks can be added if necessary
+            return true;
+        }
     }
 
     class Utas
