9f300abf837feb1cc6de7214539b10750a2aa7bb
Kevin <arpaskevin11@gmail.com>
2024-09-18 08:36:30 +0200

    Began finetuning the program. WPF Frame under reimplementation using Grid System.

--- EUtazas2020GUI/MainWindow.xaml.cs
+++ EUtazas2020GUI/MainWindow.xaml.cs
@@ -20,163 +20,163 @@ namespace EUtazas2020GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<Utas> passengers { get; set; }
+        //public List<Utas> passengers { get; set; }
 
         private const string fileSrc = @"..\..\..\src\utasadat.txt";
         public MainWindow()
         {
             InitializeComponent();
-            leaseRadioButton.IsChecked = true;
+            //leaseRadioButton.IsChecked = true;
 
-            passengers = new List<Utas>();
+            //passengers = new List<Utas>();
 
-            foreach (var item in File.ReadAllLines(fileSrc))
-            {
-                passengers.Add(new(item));
-            }
+            //foreach (var item in File.ReadAllLines(fileSrc))
+            //{
+            //    passengers.Add(new(item));
+            //}
 
-            busStopNumberCombobox.Items.Add("Válasszon megállót!"); busStopNumberCombobox.SelectedIndex = 0;
+            //busStopNumberCombobox.Items.Add("Válasszon megállót!"); busStopNumberCombobox.SelectedIndex = 0;
 
-            foreach (var item in passengers.Select(e => e.BusStopNumber).Distinct())
-            {
-                busStopNumberCombobox.Items.Add(item);
-            }
+            //foreach (var item in passengers.Select(e => e.BusStopNumber).Distinct())
+            //{
+            //    busStopNumberCombobox.Items.Add(item);
+            //}
 
-            ticketTypeCombobox.Items.Add("Válasszon megállót!"); ticketTypeCombobox.SelectedIndex = 0;
+            //ticketTypeCombobox.Items.Add("Válasszon megállót!"); ticketTypeCombobox.SelectedIndex = 0;
 
-            foreach (var item in passengers.Select(e => e.TicketType).Distinct())
-            {
-                ticketTypeCombobox.Items.Add(item);
-            }
+            //foreach (var item in passengers.Select(e => e.TicketType).Distinct())
+            //{
+            //    ticketTypeCombobox.Items.Add(item);
+            //}
 
         }
 
         private void cardIDTextbox_TextChanged(object sender, EventArgs e)
         {
-            if (cardIDTextbox.Text != null) cardIdLabel.Content = $"{cardIDTextbox.Text.Length} db";
+            //cardIdLabel.Content = $"{cardIDTextbox.Text.Length} db";
 
-            cardIdLabel.Foreground = cardIDTextbox.Text.Length > 7 ? Brushes.Red : SystemColors.WindowTextBrush;
+            //cardIdLabel.Foreground = cardIDTextbox.Text.Length > 7 ? Brushes.Red : SystemColors.WindowTextBrush;
         }   
 
         private void usableTicketCountSlider_ValueChanged(object sender, EventArgs e)
         {
-            if(usableTicketCountLabel != null) usableTicketCountLabel.Content = $"{usableTicketCountSlider.Value} db";
+            //if(usableTicketCountLabel != null) usableTicketCountLabel.Content = $"{usableTicketCountSlider.Value} db";
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            var isLeaseChecked = (sender as RadioButton) == leaseRadioButton;
+            //var isLeaseChecked = (sender as RadioButton) == leaseRadioButton;
 
-            leaseGroupbox.Visibility = isLeaseChecked ? Visibility.Visible : Visibility.Collapsed;
-            ticketGroupbox.Visibility = isLeaseChecked ? Visibility.Collapsed : Visibility.Visible;
+            //leaseGroupbox.Visibility = isLeaseChecked ? Visibility.Visible : Visibility.Collapsed;
+            //ticketGroupbox.Visibility = isLeaseChecked ? Visibility.Collapsed : Visibility.Visible;
         }
 
         private void dataRecordButton_Click(object sender, RoutedEventArgs e)
         {
-            var selectedBusStopNumber = busStopNumberCombobox.SelectedItem;
-            var cardId = cardIDTextbox.Text;
-            var selectedLeaseType = ticketTypeCombobox.SelectedItem;
+        //    var selectedBusStopNumber = busStopNumberCombobox.SelectedItem;
+        //    var cardId = cardIDTextbox.Text;
+        //    var selectedLeaseType = ticketTypeCombobox.SelectedItem;
 
 
 
-            var busstopnumberplaceholder = busStopNumberCombobox.Items[0];
-            if (selectedBusStopNumber == null || selectedBusStopNumber == busstopnumberplaceholder)
-            {
-                MessageBox.Show("Nem választott megállót!", "Hiba!");
-                return;
-            }
-            DateTime takeOffDate;
-            if (!DateTime.TryParse(takeOffDate_DatePicker.Text, out takeOffDate))
-            {
-                MessageBox.Show("Nem adott meg felszállási dátumot, vagy a dátum formátuma nem helyes!", "Hiba!");
-                return;
-            }
-            TimeSpan takeOffTime;
-            if (!TimeSpan.TryParse(takeOffTimeTextbox.Text, out takeOffTime))
-            {
-                MessageBox.Show("Az felszállás időpontja nem helyes! Használja a 'hh:mm' formátumot.", "Hiba!");
-                return;
-            }
-            if (cardId.Length != 7 || !int.TryParse(cardId, out _))
-            {
-                MessageBox.Show("Egy 7 számjegyet tartalmazó kártya azonosítót adjon meg.", "Hiba!");
-                return;
-            }
+        //    var busstopnumberplaceholder = busStopNumberCombobox.Items[0];
+        //    if (selectedBusStopNumber == null || selectedBusStopNumber == busstopnumberplaceholder)
+        //    {
+        //        MessageBox.Show("Nem választott megállót!", "Hiba!");
+        //        return;
+        //    }
+        //    DateTime takeOffDate;
+        //    if (!DateTime.TryParse(takeOffDate_DatePicker.Text, out takeOffDate))
+        //    {
+        //        MessageBox.Show("Nem adott meg felszállási dátumot, vagy a dátum formátuma nem helyes!", "Hiba!");
+        //        return;
+        //    }
+        //    TimeSpan takeOffTime;
+        //    if (!TimeSpan.TryParse(takeOffTimeTextbox.Text, out takeOffTime))
+        //    {
+        //        MessageBox.Show("Az felszállás időpontja nem helyes! Használja a 'hh:mm' formátumot.", "Hiba!");
+        //        return;
+        //    }
+        //    if (cardId.Length != 7 || !int.TryParse(cardId, out _))
+        //    {
+        //        MessageBox.Show("Egy 7 számjegyet tartalmazó kártya azonosítót adjon meg.", "Hiba!");
+        //        return;
+        //    }
 
-            var tickettypeplaceholder = ticketTypeCombobox.Items[0];
+        //    var tickettypeplaceholder = ticketTypeCombobox.Items[0];
 
-            if (leaseRadioButton.IsChecked == true)
-            {
-                if (selectedLeaseType == null || selectedLeaseType == tickettypeplaceholder)
-                {
-                    MessageBox.Show("Nem választott bérletet!", "Hiba!");
-                    return;
-                }
-                if (ticketValidityDate_Datepicker.SelectedDate == null)
-                {
-                    MessageBox.Show("Nem adott meg bérlet dátumot!", "Hiba!");
-                    return;
-                }
-            }
+        //    if (leaseRadioButton.IsChecked == true)
+        //    {
+        //        if (selectedLeaseType == null || selectedLeaseType == tickettypeplaceholder)
+        //        {
+        //            MessageBox.Show("Nem választott bérletet!", "Hiba!");
+        //            return;
+        //        }
+        //        if (ticketValidityDate_Datepicker.SelectedDate == null)
+        //        {
+        //            MessageBox.Show("Nem adott meg bérlet dátumot!", "Hiba!");
+        //            return;
+        //        }
+        //    }
             
 
-            DateTime combinedDateTime = takeOffDate.Date + takeOffTime;
+        //    DateTime combinedDateTime = takeOffDate.Date + takeOffTime;
 
-            string combinedDateTimeFormat = combinedDateTime.ToString("yyyyMMdd-HHmm");
-            string ticketValidityDateFormat = string.Empty;
-            if (ticketValidityDate_Datepicker.SelectedDate != null) ticketValidityDateFormat = ticketValidityDate_Datepicker.SelectedDate.Value.ToString("yyyyMMdd");
+        //    string combinedDateTimeFormat = combinedDateTime.ToString("yyyyMMdd-HHmm");
+        //    string ticketValidityDateFormat = string.Empty;
+        //    if (ticketValidityDate_Datepicker.SelectedDate != null) ticketValidityDateFormat = ticketValidityDate_Datepicker.SelectedDate.Value.ToString("yyyyMMdd");
 
-            string txtAppendLine = string.Empty;
-            if (leaseRadioButton.IsChecked == true)
-            {
-                txtAppendLine = $"{selectedBusStopNumber} {combinedDateTimeFormat} {cardId} {selectedLeaseType} {ticketValidityDateFormat}";
-            }
-            else
-            {
-                txtAppendLine = $"{selectedBusStopNumber} {combinedDateTimeFormat} {cardId} JGY {usableTicketCountSlider.Value}";
-            }
+        //    string txtAppendLine = string.Empty;
+        //    if (leaseRadioButton.IsChecked == true)
+        //    {
+        //        txtAppendLine = $"{selectedBusStopNumber} {combinedDateTimeFormat} {cardId} {selectedLeaseType} {ticketValidityDateFormat}";
+        //    }
+        //    else
+        //    {
+        //        txtAppendLine = $"{selectedBusStopNumber} {combinedDateTimeFormat} {cardId} JGY {usableTicketCountSlider.Value}";
+        //    }
 
-            using StreamWriter sw = new StreamWriter(fileSrc, true);
+        //    using StreamWriter sw = new StreamWriter(fileSrc, true);
 
-            sw.WriteLine(txtAppendLine);
+        //    sw.WriteLine(txtAppendLine);
 
-            MessageBox.Show("A felszállás tárolása sikeres volt!", "EUtazás 2020");
+        //    MessageBox.Show("A felszállás tárolása sikeres volt!", "EUtazás 2020");
 
-            busStopNumberCombobox.SelectedIndex = 0;
-            takeOffDate_DatePicker.SelectedDate = null;
-            takeOffTimeTextbox.Clear();
-            cardIDTextbox.Clear();
-            ticketTypeCombobox.SelectedIndex = 0;
-            ticketValidityDate_Datepicker.SelectedDate = null;
-            usableTicketCountSlider.Value = 0;
+        //    busStopNumberCombobox.SelectedIndex = 0;
+        //    takeOffDate_DatePicker.SelectedDate = null;
+        //    takeOffTimeTextbox.Clear();
+        //    cardIDTextbox.Clear();
+        //    ticketTypeCombobox.SelectedIndex = 0;
+        //    ticketValidityDate_Datepicker.SelectedDate = null;
+        //    usableTicketCountSlider.Value = 0;
         }
 
         private void newwindowButton_Click(object sender, RoutedEventArgs e)
         {
-            Window2 subWindow = new Window2(passengers);
-            subWindow.Show();
-            this.Hide();
+            //Window2 subWindow = new Window2(passengers);
+            //subWindow.Show();
+            //this.Hide();
         }
     }
 
-    public class Utas
-    {
-        public int BusStopNumber { get; set; }
-        public string TakeOffDate { get; set; }
-        public int CardId { get; set; }
-        public string TicketType { get; set; }
-        public string CardValidityPeriod { get; set; }
-        public Utas(string sor) {
-            var x = sor.Split(" ");
-            BusStopNumber = Convert.ToInt32(x[0]);
-            TakeOffDate = x[1];
-            CardId = Convert.ToInt32(x[2]);
-            TicketType = x[3];
-            CardValidityPeriod = x[4];
-        }
-        public override string ToString()
-        {
-            return $"Bus stop number: {BusStopNumber}, Take-off date: {TakeOffDate}, Card ID: {CardId}, Ticket type: {TicketType}, Card validity period: {CardValidityPeriod}";
-        }
-    }
+    //public class Utas
+    //{
+    //    public int BusStopNumber { get; set; }
+    //    public string TakeOffDate { get; set; }
+    //    public int CardId { get; set; }
+    //    public string TicketType { get; set; }
+    //    public string CardValidityPeriod { get; set; }
+    //    public Utas(string sor) {
+    //        var x = sor.Split(" ");
+    //        BusStopNumber = Convert.ToInt32(x[0]);
+    //        TakeOffDate = x[1];
+    //        CardId = Convert.ToInt32(x[2]);
+    //        TicketType = x[3];
+    //        CardValidityPeriod = x[4];
+    //    }
+    //    public override string ToString()
+    //    {
+    //        return $"Bus stop number: {BusStopNumber}, Take-off date: {TakeOffDate}, Card ID: {CardId}, Ticket type: {TicketType}, Card validity period: {CardValidityPeriod}";
+    //    }
+    //}
 }
--- EUtazas2020GUI/Window2.xaml.cs
+++ EUtazas2020GUI/Window2.xaml.cs
@@ -1,70 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
 
-namespace EUtazas2020GUI
-{
-    /// <summary>
-    /// Interaction logic for Window2.xaml
-    /// </summary>
-    public partial class Window2 : Window
-    {
-        private List<Utas> passengersConsole;
-        public Window2(List<Utas> passengers)
-        {
-            InitializeComponent();
+//namespace EUtazas2020GUI
+//{
+//    /// <summary>
+//    /// Interaction logic for Window2.xaml
+//    /// </summary>
+//    public partial class Window2 : Window
+//    {
+//        private List<Utas> passengersConsole;
+//        public Window2(List<Utas> passengers)
+//        {
+//            InitializeComponent();
 
-            this.passengersConsole = passengers;
+//            this.passengersConsole = passengers;
 
-            lbl1.Content = $"3. feladat\nA buszra {passengersConsole.Count()} utas akart felszállni.";
-            lbl2.Content = $"4. feladat\nA buszra {passengersConsole.Count(p => p.CardValidityPeriod == "0" ||
-                (DateTime.TryParseExact(p.CardValidityPeriod, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out var cardValidityDate) &&
-                 DateTime.TryParseExact(p.TakeOffDate, "yyyyMMdd-HHmm", null, System.Globalization.DateTimeStyles.None, out var takeOffDate) &&
-                 takeOffDate > cardValidityDate && takeOffDate.Date != cardValidityDate.Date))} utas nem szállhatott fel.";
+//            lbl1.Content = $"3. feladat\nA buszra {passengersConsole.Count()} utas akart felszállni.";
+//            lbl2.Content = $"4. feladat\nA buszra {passengersConsole.Count(p => p.CardValidityPeriod == "0" ||
+//                (DateTime.TryParseExact(p.CardValidityPeriod, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out var cardValidityDate) &&
+//                 DateTime.TryParseExact(p.TakeOffDate, "yyyyMMdd-HHmm", null, System.Globalization.DateTimeStyles.None, out var takeOffDate) &&
+//                 takeOffDate > cardValidityDate && takeOffDate.Date != cardValidityDate.Date))} utas nem szállhatott fel.";
 
 
-            var f5 = passengersConsole.GroupBy(p => p.BusStopNumber).MaxBy(g => g.Count());
+//            var f5 = passengersConsole.GroupBy(p => p.BusStopNumber).MaxBy(g => g.Count());
 
-            lbl3.Content = $"5. feladat\nA legtöbb utas ({f5.Key} fő) a {f5.Count()}. megállóban próbált felszállni.";
+//            lbl3.Content = $"5. feladat\nA legtöbb utas ({f5.Key} fő) a {f5.Count()}. megállóban próbált felszállni.";
 
-            var f6Ervenyesek = passengersConsole.Where(p => int.Parse(p.CardValidityPeriod) != 0);
-            var f6Ingyenes = f6Ervenyesek.Count(p => p.TicketType == "NYP" || p.TicketType == "RVS" || p.TicketType == "GYK");
-            var f6Kedvezmenyes = f6Ervenyesek.Count(p => p.TicketType == "TAB" || p.TicketType == "NYB");
+//            var f6Ervenyesek = passengersConsole.Where(p => int.Parse(p.CardValidityPeriod) != 0);
+//            var f6Ingyenes = f6Ervenyesek.Count(p => p.TicketType == "NYP" || p.TicketType == "RVS" || p.TicketType == "GYK");
+//            var f6Kedvezmenyes = f6Ervenyesek.Count(p => p.TicketType == "TAB" || p.TicketType == "NYB");
 
-            lbl4.Content = $"6. feladat\nIngyenesen utazók száma: {f6Ingyenes} fő\nA kedvezményesen utazók száma: {f6Kedvezmenyes} fő";
+//            lbl4.Content = $"6. feladat\nIngyenesen utazók száma: {f6Ingyenes} fő\nA kedvezményesen utazók száma: {f6Kedvezmenyes} fő";
 
-            var f7Berlet = passengersConsole
-             .Where(p => DateTime.TryParseExact(p.CardValidityPeriod, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out _))
-             .Select(p => new
-             {
-                 CardID = p.CardId,
-                 TakeOffDate = DateTime.ParseExact(p.TakeOffDate, "yyyyMMdd-HHmm" ,null),
-                 CardValidityPeriod = DateTime.ParseExact(p.CardValidityPeriod, "yyyyMMdd", null)
-             }).ToList();
+//            var f7Berlet = passengersConsole
+//             .Where(p => DateTime.TryParseExact(p.CardValidityPeriod, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out _))
+//             .Select(p => new
+//             {
+//                 CardID = p.CardId,
+//                 TakeOffDate = DateTime.ParseExact(p.TakeOffDate, "yyyyMMdd-HHmm" ,null),
+//                 CardValidityPeriod = DateTime.ParseExact(p.CardValidityPeriod, "yyyyMMdd", null)
+//             }).ToList();
 
-            using StreamWriter sw = new(@"..\..\..\src\figyelmeztetes.txt", false);
+//            using StreamWriter sw = new(@"..\..\..\src\figyelmeztetes.txt", false);
 
-            foreach (var item in f7Berlet)
-            {
-                var expiryTime = (item.CardValidityPeriod - item.TakeOffDate);
+//            foreach (var item in f7Berlet)
+//            {
+//                var expiryTime = (item.CardValidityPeriod - item.TakeOffDate);
 
-                if (expiryTime.TotalDays <= 3)
-                {
-                    sw.WriteLine($"{item.CardID} {item.CardValidityPeriod.ToString("yyyy-MM-dd")}");
-                }
-            }
-        }
-    }
-}
+//                if (expiryTime.TotalDays <= 3)
+//                {
+//                    sw.WriteLine($"{item.CardID} {item.CardValidityPeriod.ToString("yyyy-MM-dd")}");
+//                }
+//            }
+//        }
+//    }
+//}
